38843d7308eb59839dfd05c22c7af0b14dbccbff
Rajib Singh <rajibsingh@yahoo.com>
2017-01-04 21:45:38 -0500

    more authors, tweet update

--- data/bin/dump.sh
+++ data/bin/dump.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 mongoexport --db tweetsdb --collection tweets --out data/dumps/tweets.json
+mongoexport --db tweetsdb --collection authors --out data/dumps/authors.json
--- data/bin/load.sh
+++ data/bin/load.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 mongoimport --db test --collection tweets --file data/dumps/tweets.json
+mongoimport --db test --collection authors --file data/dumps/authors.json
